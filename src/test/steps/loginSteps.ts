ed31557596351a4211b935f787fae470a01844fe
Juan Piza Pinzon <jpizapinzon@splunk.com>
2024-12-24 10:13:09 -0500

    Add pages object pattern and update loginSteps with this pattern

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -1,19 +1,36 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { pageFixture } from "../../hooks/pageFixture";
+import { LandingPage, SignInPage, HomePage, SettingsPage } from "../pages";
 
 /*let browser: Browser;
 let page: Page;*/
 
+/* We need to declare the objects globally but this will cause an issue.
+ cause the objects will be undefined for the scope
+ cannot read properties of undefined reading locator
+*/
+let landingPage: LandingPage;
+let singInPage: SignInPage;
+let homePage: HomePage;
+let settingsPage: SettingsPage;
+
 Given("I am on the conduit login page", async function () {
   /* browser = await chromium.launch({ headless: false });
   page = await browser.newPage();*/
-  await pageFixture.page.goto("https://react-redux.realworld.io/");
-  await pageFixture.page.locator("//a[normalize-space()='Sign in']").click();
+  /*await pageFixture.page.goto("https://react-redux.realworld.io/");
+  await pageFixture.page.locator("//a[normalize-space()='Sign in']").click();*/
+  /** In the Given statement it is recommended to declare the variables */
+  landingPage = new LandingPage(pageFixture.page);
+  singInPage = new SignInPage(pageFixture.page);
+  homePage = new HomePage(pageFixture.page);
+  settingsPage = new SettingsPage(pageFixture.page);
+  await landingPage.navigateTo("https://react-redux.realworld.io/");
+  await landingPage.clickSignInButton();
 });
 
 When("I login with valid credentials", async function () {
-  const emailInput = pageFixture.page.locator("input[placeholder='Email']");
+  /*const emailInput = pageFixture.page.locator("input[placeholder='Email']");
   const passwordInput = pageFixture.page.locator(
     "input[placeholder='Password']"
   );
@@ -21,21 +38,28 @@ When("I login with valid credentials", async function () {
 
   await emailInput.fill("playwrightdemo@gmail.com");
   await passwordInput.fill("playwrightdemo");
-  await signInButton.click();
+  await signInButton.click();*/
+  await singInPage.signInWithEmailAndPassword(
+    "playwrightdemo@gmail.com",
+    "playwrightdemo"
+  );
 });
 
 When("I click on the settings button", async function () {
-  await pageFixture.page.locator("a[href='#settings']").click();
+  // await pageFixture.page.locator("a[href='#settings']").click();
+  await homePage.clickSettingsButton();
 });
 
 When("I click on the logout button", async function () {
-  await pageFixture.page
+  /*await pageFixture.page
     .locator("//button[normalize-space()='Or click here to logout.']")
-    .click();
+    .click();*/
+  await settingsPage.clickLogoutButton();
 });
 
 Then("I route back to the login page", async function () {
-  await expect(
+  /*await expect(
     pageFixture.page.locator("//a[normalize-space()='Sign in']")
-  ).toBeVisible();
+  ).toBeVisible();*/
+  await landingPage.ensureSignInButtonIsVisible();
 });
